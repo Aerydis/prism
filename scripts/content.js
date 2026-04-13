2dc130f890074e38549474fa714bf3f7e7c7c1f2
Aerydis <aerydis700@gmail.com>
2026-04-13 21:41:51 +0900

    horrible first attempt

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,13 @@
-function renderReadingTime(article) {
-    if (!article) {
+// ERT: Estimated Reading Time (I am lazy)
+
+const wordCount = article.text.split(" ").length;
+const ERT = wordCount / 400
+
+const ERTbox = document.createElement('div')
+ERTbox.textContent = `Estimated reading time: ${ERT}`
+
+function renderERT() {
+    if (!article) || (!h1) {
         return;
     }
 }
-
-const text = article.textContent;
-const wordMatchRegExp = /[^\s]+/g
-const words = text.matchAll(wordMatchRegExp)
